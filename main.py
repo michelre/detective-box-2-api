6c943c2215595ab10132f297940792cb231aa3f1
Rémi Michel <remi.michel38@gmail.com>
2023-06-26 23:09:24 +0200

    Revert "feat(): add cors"
    This reverts commit e57e59f3911f357cc337be330ac3ea3ba180decd.

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
 from fastapi import FastAPI
 from schemas import Status
 from config import Settings
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 settings = Settings()
-
-origins = [
-    "https://api.detectivebox.remimichel.fr",
-    "http://localhost:5173",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 @app.get('/status')
