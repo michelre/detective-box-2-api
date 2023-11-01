aa121fc4b4c2d7ce101879ac3f314461b1e05da1
Rémi Michel <remi.michel38@gmail.com>
2023-11-01 21:54:10 +0100

    feat(): history with auth

--- api/models/history.py
+++ api/models/history.py
@@ -1,8 +1,16 @@
-from sqlalchemy import Column, Integer, String, Text, ForeignKey, Boolean, JSON
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, JSON
 from sqlalchemy.orm import relationship, Mapped
 
 from api.database import Base
 from api.models.box import Box
+
+
+class HistoryUser(Base):
+    __tablename__ = "history_user"
+    user_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
+    history_id = Column(Integer, ForeignKey("history.id"), primary_key=True)
+    ref_data = Column(String)
+    status = Column(Boolean)
 
 
 class History(Base):
--- api/routers/history.py
+++ api/routers/history.py
@@ -1,47 +1,64 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Depends, HTTPException, status
-from typing import Annotated, List
 from sqlalchemy.orm import Session
-from sqlalchemy.orm.attributes import flag_modified
 
 from api.database import get_db
 from api.models import history as history_models
+from api.schemas import history as history_schemas
 from api.utils import auth as auth_utils
-
-from api.schemas.history import History
 
 router = APIRouter(prefix="/history")
 
 
 @router.get(path='/{box_id}')
 def get_by_box(
-        # user_id: Annotated[int, Depends(auth_utils.get_connected_user_id)],
-        box_id: str,
+        user_id: Annotated[int, Depends(auth_utils.get_connected_user_id)],
+        box_id: int,
         db: Session = Depends(get_db),
 ):
-    return db.query(history_models.History) \
+    history = db \
+        .query(history_models.History) \
         .filter_by(box_id=box_id) \
-        .all()
+        .first()
+
+    for d in history.data:
+        exists = db\
+            .query(history_models.HistoryUser)\
+            .filter_by(history_id=history.id)\
+            .filter_by(user_id=user_id)\
+            .filter_by(ref_data=d['id'])\
+            .first()
+
+        if exists:
+            d['status'] = exists.status
+
+    return history
 
 
 @router.put(path='/reset')
 def reset(
+        user_id: Annotated[int, Depends(auth_utils.get_connected_user_id)],
         db: Session = Depends(get_db),
 ):
-    data = db.query(history_models.History).all()
+    data = db.query(history_models.HistoryUser) \
+        .filter_by(user_id=user_id) \
+        .all()
 
     for d in data:
-        for e in d.data:
-            e['status'] = False
+        db.delete(d)
 
-        flag_modified(d, "data")
     db.commit()
-    return "OK"
+
+    return 'OK'
 
 
 @router.put(path='/{box_id}')
 def update_status(
+        user_id: Annotated[int, Depends(auth_utils.get_connected_user_id)],
         box_id: int,
         id: str,
+        new_status: history_schemas.HistoryStatus,
         db: Session = Depends(get_db),
 ):
     data = db.query(history_models.History) \
@@ -51,10 +68,30 @@ def update_status(
     if not data:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
-    for d in data.data:
+    found = None
+    for idx, d in enumerate(data.data):
         if d['id'] == id:
-            d['status'] = True
+            found = idx
 
-    flag_modified(data, "data")
+    if found is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+    exists = db.query(history_models.HistoryUser) \
+        .filter_by(history_id=data.id) \
+        .filter_by(user_id=user_id) \
+        .filter_by(ref_data=str(id)) \
+        .first()
+
+    if not exists:
+        new = history_models.HistoryUser(
+            user_id=user_id,
+            history_id=data.id,
+            ref_data=id,
+            status=new_status.status
+        )
+        db.add(new)
+    else:
+        exists.status = new_status.status
+
     db.commit()
-    return data
+    return 'OK'
