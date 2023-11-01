992bf9bd09a675aae4714f436389b0a7875448fa
Rémi Michel <remi.michel38@gmail.com>
2023-11-01 22:08:31 +0100

    feat(): objectives with auth

--- api/models/objective.py
+++ api/models/objective.py
@@ -1,9 +1,18 @@
-from sqlalchemy import Column, Integer, ForeignKey
-from sqlalchemy.orm import relationship, Mapped
+from sqlalchemy import Column, Integer, ForeignKey, String
 from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.orm import relationship, Mapped
 
 from api.database import Base
 from api.models.box import Box
+
+
+class ObjectiveUser(Base):
+    __tablename__ = "objective_user"
+
+    user_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
+    objective_id = Column(Integer, ForeignKey("objective.id"), primary_key=True)
+    ref_data = Column(Integer)
+    status = Column(String)
 
 
 class Objective(Base):
--- api/routers/objective.py
+++ api/routers/objective.py
@@ -1,41 +1,51 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Depends, HTTPException, status
-from typing import Annotated, List
 from sqlalchemy.orm import Session
 
 from api.database import get_db
 from api.models import objective as objective_models
+from api.schemas import objective as objective_schemas
 from api.utils import auth as auth_utils
-from sqlalchemy.orm.attributes import flag_modified
-from api.utils import in_array
-
-from api.schemas.help import Help
 
 router = APIRouter(prefix="/objectives")
 
 
 @router.get(path='/')
 def get(
-        # user_id: Annotated[int, Depends(auth_utils.get_connected_user_id)],
+        user_id: Annotated[int, Depends(auth_utils.get_connected_user_id)],
         db: Session = Depends(get_db),
 ):
-    return db.query(objective_models.Objective).all()
+
+    objectives = db.query(objective_models.Objective).all()
+
+    for o in objectives:
+        for d in o.data:
+            exists = db\
+                .query(objective_models.ObjectiveUser)\
+                .filter_by(objective_id=o.id)\
+                .filter_by(user_id=user_id)\
+                .filter_by(ref_data=str(d['id']))\
+                .first()
+
+            if exists:
+                d['status'] = exists.status
+
+    return objectives
 
 
 @router.put(path='/reset')
 def reset(
-        # user_id: Annotated[int, Depends(auth_utils.get_connected_user_id)],
+        user_id: Annotated[int, Depends(auth_utils.get_connected_user_id)],
         db: Session = Depends(get_db),
 ) -> str:
-    data = db.query(objective_models.Objective).all()
+    data = db.query(objective_models.ObjectiveUser) \
+        .filter_by(user_id=user_id) \
+        .all()
 
     for d in data:
-        for e in d.data:
-            if not in_array([11, 12, 13, 22, 31, 32], e['id']):
-                e['status'] = 'closed'
-            else:
-                e['status'] = 'open'
+        db.delete(d)
 
-        flag_modified(d, "data")
     db.commit()
 
     return 'OK'
@@ -43,8 +53,10 @@ def reset(
 
 @router.put(path='/{box_id}')
 def update_status(
+        user_id: Annotated[int, Depends(auth_utils.get_connected_user_id)],
         box_id: int,
         id: int,
+        new_status: objective_schemas.ObjectiveStatus,
         db: Session = Depends(get_db),
 ):
     data = db.query(objective_models.Objective) \
@@ -54,10 +66,30 @@ def update_status(
     if not data:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
-    for d in data.data:
+    found = None
+    for idx, d in enumerate(data.data):
         if d['id'] == id:
-            d['status'] = 'closed'
+            found = idx
 
-    flag_modified(data, "data")
+    if found is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+    exists = db.query(objective_models.ObjectiveUser) \
+        .filter_by(objective_id=data.id) \
+        .filter_by(user_id=user_id) \
+        .filter_by(ref_data=str(id)) \
+        .first()
+
+    if not exists:
+        new = objective_models.ObjectiveUser(
+            user_id=user_id,
+            objective_id=data.id,
+            ref_data=id,
+            status=new_status.status
+        )
+        db.add(new)
+    else:
+        exists.status = new_status.status
+
     db.commit()
     return 'OK'
