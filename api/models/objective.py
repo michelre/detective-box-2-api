c5542d58ca495f3ae17d76a0b6503a9854521a6e
Rémi Michel <remi.michel38@gmail.com>
2023-11-01 16:06:52 +0100

    feat(): objective API

--- api/models/objective.py
+++ api/models/objective.py
@@ -1,8 +1,8 @@
-from sqlalchemy import Column, Integer, String, Text, ForeignKey, Enum
+from sqlalchemy import Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship, Mapped
+from sqlalchemy.dialects.postgresql import JSONB
 
 from api.database import Base
-from api.enums import ObjectiveStatus
 from api.models.box import Box
 
 
@@ -10,9 +10,6 @@ class Objective(Base):
     __tablename__ = "objective"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    subtitle = Column(String)
-    detail = Column(Text)
-    status = Column(Enum(ObjectiveStatus))
+    data = Column(JSONB)
     box_id = Column(Integer, ForeignKey("box.id"))
     box: Mapped[Box] = relationship("Box")
--- api/routers/objective.py
+++ api/routers/objective.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from typing import Annotated, List
 from sqlalchemy.orm import Session
 
 from api.database import get_db
 from api.models import objective as objective_models
 from api.utils import auth as auth_utils
+from sqlalchemy.orm.attributes import flag_modified
+from api.utils import in_array
 
 from api.schemas.help import Help
 
@@ -15,5 +17,47 @@ router = APIRouter(prefix="/objectives")
 def get(
         # user_id: Annotated[int, Depends(auth_utils.get_connected_user_id)],
         db: Session = Depends(get_db),
-) -> List[Help]:
+):
     return db.query(objective_models.Objective).all()
+
+
+@router.put(path='/reset')
+def reset(
+        # user_id: Annotated[int, Depends(auth_utils.get_connected_user_id)],
+        db: Session = Depends(get_db),
+) -> str:
+    data = db.query(objective_models.Objective).all()
+
+    for d in data:
+        for e in d.data:
+            if not in_array([11, 12, 13, 22, 31, 32], e['id']):
+                e['status'] = 'closed'
+            else:
+                e['status'] = 'open'
+
+        flag_modified(d, "data")
+    db.commit()
+
+    return 'OK'
+
+
+@router.put(path='/{box_id}')
+def update_status(
+        box_id: int,
+        id: int,
+        db: Session = Depends(get_db),
+):
+    data = db.query(objective_models.Objective) \
+        .filter_by(box_id=box_id) \
+        .first()
+
+    if not data:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+    for d in data.data:
+        if d['id'] == id:
+            d['status'] = 'closed'
+
+    flag_modified(data, "data")
+    db.commit()
+    return 'OK'
