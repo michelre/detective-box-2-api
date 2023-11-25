b62ab31de7f5c9b09ffa956e5c40b080d7837995
Bastien-Baloup <35816948+Bastien-Baloup@users.noreply.github.com>
2023-11-25 06:56:39 +0100

    tentative d'utilisation de gunicorn comme préconisé par DigitalOcean

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 alembic upgrade head
-python -m api
+gunicorn -b 0.0.0.0:8000 api:app &
+gunicorn -b 0.0.0.0:8001 api:app_stream &
