653f7fe0fe5972c7f21d4c9843ac5a50b6607989
Bastien-Baloup <35816948+Bastien-Baloup@users.noreply.github.com>
2023-11-25 07:11:57 +0100

    changement du type de worker pour lancer FastApi en tant que ASGI et non WSGI

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 alembic upgrade head
-gunicorn -b 0.0.0.0:8000 api:app
+gunicorn -b 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker api:app
