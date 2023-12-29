045107f61b2a3987269ef337981d53c2e4f2a12b
Bastien-Baloup <35816948+Bastien-Baloup@users.noreply.github.com>
2023-12-29 21:30:44 +0100

    Augmente la taille de la pool de connexion pour encaisser un plus grand nombre d'utilisateur

--- api/database.py
+++ api/database.py
@@ -7,7 +7,9 @@ from api.config import settings
 DATABASE_URI = settings.database_uri
 
 engine = create_engine(
-    DATABASE_URI
+    DATABASE_URI,
+    pool_size=30,
+    max_overflow=-1
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
