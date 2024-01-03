bdf6a83fbc42218ae75f3a667fdcaef138a9a459
Bastien-Baloup <35816948+Bastien-Baloup@users.noreply.github.com>
2024-01-03 19:10:18 +0100

    Passage de la pool en transactionnel au lieu d'en session

--- api/database.py
+++ api/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 from api.config import settings
 
@@ -8,11 +8,11 @@ DATABASE_URI = settings.database_uri
 
 engine = create_engine(
     DATABASE_URI,
-    pool_size=30,
+    pool_size=20,
     max_overflow=-1
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 Base = declarative_base()
 
